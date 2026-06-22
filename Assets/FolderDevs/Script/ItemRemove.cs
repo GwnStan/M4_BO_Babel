7bf6ca92d4393780197d91b34af8f4a954bed074
karlaps45 <140808213+karlaps45@users.noreply.github.com>
2026-06-22 15:45:43 +0200

    itemAddscriptdialogue

--- Assets/FolderDevs/Script/ItemRemove.cs
+++ Assets/FolderDevs/Script/ItemRemove.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 public class ItemRemove : MonoBehaviour
 {
     public string itemTag;
-
+    public inventoryItem item;
     public void Remove()
     {
         FindFirstObjectByType<Inventoryscript>().RemoveItemByTag(itemTag);
+    }
+    public void add()
+    {
+        FindAnyObjectByType<Inventoryscript>().AddItem(item);
     }
 }
