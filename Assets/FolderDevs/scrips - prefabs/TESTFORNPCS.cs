cf5b54a9e2e81494a5c2019d74f5ca60f8cfc03d
KivinK <klinkenbergkf@gmail.com>
2026-07-23 19:35:21 +0200

    PhonoShift - Clean up / with Prefabs

---
+++ Assets/FolderDevs/scrips - prefabs/TESTFORNPCS.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class TESTFORNPCS : MonoBehaviour
+{
+    public GameObject eventobject;
+
+    public void deathofme()
+    {
+        Debug.Log("HIIIIIIIIIIIIIMDEAAAAAADNOOOOOOOOOOOOO");
+
+    }
+
+}
---
+++ Assets/Resources/items/Subject.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+
+
+[CreateAssetMenu(fileName = "New Subject", menuName = "Create new Subject")]
+public class Subject : ScriptableObject
+{
+    public string subjectname;
+    public Color bordercolor;
+    public Color innercolor;
+    public Sprite subjectface;
+    public Sprite subjectborder;
+    public Sprite subjectinner;
+}
